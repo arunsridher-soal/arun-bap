af5df51ab4142dee56981606157a7c186c32d5ec
arunsridher <arunsridher@gmail.com>
2023-09-13 21:06:40 +0530

    added a get route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ import Express from "express";
 import { on_select } from "../controllers/index.js";
 const router = Express.Router();
 
+router.get("/", (req, res) => {
+  res.send("Up & running...");
+});
 router.post("/on_select", on_select);
 
 export default router;
